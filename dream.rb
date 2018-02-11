9fcaacb19a9768b03c72c91fcdb27b3a8c3f1e83
Takahide Nogayama <nogayama@jp.ibm.com>
2018-02-11 18:32:55 +0900

    Initial commit

---
+++ dream.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+class Dream < Formula
+  desc "Dream is a sleep like command"
+  homepage "https://github.com/nogayama/dream"
+  url "https://github.com/nogayama/homebrew-dream.git", :tag => "v0.0.3"
+  version "0.0.3"
+  # sha256 ""
+  def install
+    bin.install "bin/dream"
+  end
+  test do
+    system "false"
+  end
+end
