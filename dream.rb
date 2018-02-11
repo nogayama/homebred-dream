d3e1042511baa63f4c6ea8c5c887681028134726
Takahide Nogayama <nogayama@jp.ibm.com>
2018-02-11 19:25:52 +0900

    Update to v0.0.6

--- dream.rb
+++ dream.rb
@@ -2,10 +2,12 @@
 # -*- coding: utf-8 -*-
 
 class Dream < Formula
+  depends_on "bash" # v4
+  
   desc "Dream is a sleep like command"
   homepage "https://github.com/nogayama/dream"
-  url "https://github.com/nogayama/homebrew-dream.git", :tag => "v0.0.5"
-  version "0.0.5"
+  url "https://github.com/nogayama/homebrew-dream.git", :tag => "v0.0.6"
+  version "0.0.6"
   # sha256 ""
   def install
     bin.install "bin/dream"
